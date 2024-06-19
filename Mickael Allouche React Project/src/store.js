4f0f4001b5b48d36dd41b7f5d3f16f92806ad88d
Mickael <allouchemicka@gmail.com>
2024-06-19 22:41:50 +0300

    First Commit

---
+++ Mickael Allouche React Project/src/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import TodosSlice from './components/learning/todos/TodosSlice'; 
+
+export default configureStore({
+    reducer: {
+        todos: TodosSlice,
+    }
+});
+
